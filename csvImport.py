3e4d05557db508078f1c93da7a2b26d14dcda2e1
EMA\bkt5031 <bkt5031@psu.edu>
2018-12-11 16:55:13 -0500

    tbc

--- csvImport.py
+++ csvImport.py
@@ -1,20 +1,20 @@
 import pyodbc
 import csv
+import logging
 
 # DESTINATION CONNECTION
-# drivr = "ODBC Driver 13 for SQL Server"
-# servr = "uao-nebula"
-# db = "Staging"
-# username = ""
-# password = ""
-# my_cnxn = pyodbc.connect('DRIVER={};SERVER={};DATABASE={};'.format(drivr,servr,db))
+ 
 my_cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER=uao-nebula; DATABASE=Staging; Trusted_Connection=yes')
 my_cursor = my_cnxn.cursor()
+ 
 
 def insert_records(table, yourcsv, cursor, cnxn):
     #INSERT SOURCE RECORDS TO DESTINATION
     with open(yourcsv) as csvfile:
         csvFile = csv.reader(csvfile, delimiter=',')
+        print('File Read')
+        logging.info('File Read')
+
         header = next(csvFile)
         headers = map((lambda x: x.strip()), header)
         insert = 'INSERT INTO {} ('.format(table) + ', '.join(headers) + ') VALUES '
@@ -22,8 +22,12 @@ def insert_records(table, yourcsv, cursor, cnxn):
             values = map((lambda x: "'"+x.strip()+"'"), row)
             my_cursor.execute(insert +'('+ ', '.join(values) +');' )
             my_cnxn.commit() #must commit unless your sql database auto-commits
+            logging.info('Values Inserted') 
+            print('Values Inserted')
 
 table = '[STAGING].[dbo].TalismaLeadCards'
-mycsv = 'CommonApp_Prospect.txt' # SET YOUR FILEPATH
+mycsv = 'test.csv' # SET YOUR FILEPATH
 insert_records(table, mycsv, my_cursor, my_cnxn)
 my_cursor.close()
+logging.info('Data Successfully Inserted')
+print('Data Successfully Inserted')
